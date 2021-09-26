b85cde9618cbb619e3205423584774084a43d219
Matheus Nicolay Ulharuso Wiener <matheuswiener9@gmail.com>
2021-09-26 17:37:44 -0300

    Developed delete by id operation

--- src/main/java/one/dio/personapi/service/PersonService.java
+++ src/main/java/one/dio/personapi/service/PersonService.java
@@ -36,7 +36,6 @@ public class PersonService {
                 .build();
     }
 
-    @GetMapping
     public List<PersonDTO> listAll() {
         List<Person> allPeople = personRepository.findAll();
         return allPeople.stream()
@@ -45,10 +44,18 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id)
-                .orElseThrow(() -> new PersonNotFoundException(id));
-
+        Person person = verifyIfExists(id);
         return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
     }
 }
 
